5698db703a6e90c20f1aae2d614da82a6d3605a4
juanjose <juanjoperez2121@gmail.com>
2024-08-28 09:47:26 -0500

    se arreglan las direcciones de los imports

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import './Header.css';
+import accountIcon from '../../assets/icons/account.png';
+import cartIcon from '../../assets/icons/carrito.png';
+import logo from '../../assets/images/misaklogo.png'; 
+import menuIcon from '../../assets/icons/menu.png';
 
 const Header = () => {
   return (
     <header className="header">
-      <div className="header-content">
-        <button className="menu-button">☰</button>
-        <h1 className="logo">Misakuuamboshop</h1>
+       <img className="menu-icon" src={menuIcon} alt="Menú" />
+       <img className="logo" src={logo} alt="Logo Misak" />
         <div className="search-bar">
           <input type="text" placeholder="mochilas en lana" />
           <button type="submit">🔍</button>
         </div>
         <div className="user-actions">
-          <button className="account-button">👤 Mi cuenta</button>
-          <button className="cart-button">🛒 0</button>
-        </div>
+        <button className="account-button">
+          <img src={accountIcon} alt="Mi cuenta" />
+          Mi cuenta
+        </button>
+        <button className="cart-button">
+          <img src={cartIcon} alt="Carrito" />
+          0
+        </button>
       </div>
     </header>
   );
