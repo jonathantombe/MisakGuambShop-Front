c251b10fd516c62ecc6994ce71a56765c51aa1b2
jonathantombe <jhontombe33@gmail.com>
2024-08-28 07:42:37 -0500

    Añadidas direcciones de navegación al logo, registrarse y login en el header J.T.Y

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header-content">
         <button className="menu-button">☰</button>
-        <h1 className="logo">Misakuuamboshop</h1>
+        <div className="logo">
+          <Link to="/">MisakGuambShop</Link>
+        </div>
         <div className="search-bar">
           <input type="text" placeholder="mochilas en lana" />
           <button type="submit">🔍</button>
         </div>
-        <div className="user-actions">
-          <button className="account-button">👤 Mi cuenta</button>
-          <button className="cart-button">🛒 0</button>
-        </div>
+        <nav className="nav-links">
+          <Link className="account-button">👤</Link>
+          <Link to="/login" className="nav-link">Iniciar Sesión</Link>
+          <Link to="/register" className="nav-link">Registrarse</Link>
+          <Link className="cart-button">🛒 0</Link>
+        </nav>
       </div>
     </header>
   );
