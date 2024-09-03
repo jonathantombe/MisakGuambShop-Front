d66d497fdf55d23b6e84af986d065f7c59d8e294
juanjose <juanjoperez2121@gmail.com>
2024-09-03 11:36:28 -0500

    Se soluciona el problema del punto en las cards de home

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -6,40 +6,40 @@ const NotFound = () => {
     <main>
       <div className="not-found-404">404</div>
       <div className="box-of-star1">
-        <div className="star star-position1"></div>
-        <div className="star star-position2"></div>
-        <div className="star star-position3"></div>
-        <div className="star star-position4"></div>
-        <div className="star star-position5"></div>
-        <div className="star star-position6"></div>
-        <div className="star star-position7"></div>
+        <div className="point point-position1"></div>
+        <div className="point point-position2"></div>
+        <div className="point point-position3"></div>
+        <div className="point point-position4"></div>
+        <div className="point point-position5"></div>
+        <div className="point point-position6"></div>
+        <div className="point point-position7"></div>
       </div>
       <div className="box-of-star2">
-        <div className="star star-position1"></div>
-        <div className="star star-position2"></div>
-        <div className="star star-position3"></div>
-        <div className="star star-position4"></div>
-        <div className="star star-position5"></div>
-        <div className="star star-position6"></div>
-        <div className="star star-position7"></div>
+        <div className="point point-position1"></div>
+        <div className="point point-position2"></div>
+        <div className="point point-position3"></div>
+        <div className="point point-position4"></div>
+        <div className="point point-position5"></div>
+        <div className="point point-position6"></div>
+        <div className="point point-position7"></div>
       </div>
       <div className="box-of-star3">
-        <div className="star star-position1"></div>
-        <div className="star star-position2"></div>
-        <div className="star star-position3"></div>
-        <div className="star star-position4"></div>
-        <div className="star star-position5"></div>
-        <div className="star star-position6"></div>
-        <div className="star star-position7"></div>
+        <div className="point point-position1"></div>
+        <div className="point point-position2"></div>
+        <div className="point point-position3"></div>
+        <div className="point point-position4"></div>
+        <div className="point point-position5"></div>
+        <div className="point point-position6"></div>
+        <div className="point point-position7"></div>
       </div>
       <div className="box-of-star4">
-        <div className="star star-position1"></div>
-        <div className="star star-position2"></div>
-        <div className="star star-position3"></div>
-        <div className="star star-position4"></div>
-        <div className="star star-position5"></div>
-        <div className="star star-position6"></div>
-        <div className="star star-position7"></div>
+        <div className="point point-position1"></div>
+        <div className="point point-position2"></div>
+        <div className="point point-position3"></div>
+        <div className="point point-position4"></div>
+        <div className="point point-position5"></div>
+        <div className="point point-position6"></div>
+        <div className="point point-position7"></div>
       </div>
       <div data-js="astro" className="astronaut">
         <div className="head"></div>
