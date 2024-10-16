9a4f9e09d977ca9851da5c7ebc09587d7879e581
juanjose <juanjoperez2121@gmail.com>
2024-10-16 11:14:21 -0500

    Se implementa la pagina del carrito de compras

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,11 @@ import Register from './pages/Register/Register';
 import { AuthProvider } from './context/AuthContext';
 import Login from './pages/Login/Login';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import UserProfile from './components/UserProfile/Userprofile'
 import UserProfileEdit from './components/UserProfile/UserProfileEdit'
 import ResetPassword from './pages/ResetPassword/ResetPassword';
 import ResetPasswordForm from './pages/ResetPassword/ResetPasswordForm'
-import HeroMisakWelcome from '../src/components/HeroMisakWelcome/HeroMisakWelcome';
 import History from '../src/pages/History/History';
 import ProductSearch from './pages/SellProduct/ProductSearch';
 import CategoryConfirmation from './pages/SellProduct/CategoryConfirmation'
@@ -22,6 +22,7 @@ import NotFound from './pages/NotFound/NotFound';
 import Contact from './pages/Contact/Contact';
 import FAQ from './pages/CustomerServices/FAQ/FAQ';
 import JoinUs from './pages/CustomerServices/JoinUs/JoinUs';
+import CartPage from '../src/pages/CartPage/CartPage';     
 
 const App = () => {
   return (
@@ -44,7 +45,11 @@ const App = () => {
             <Route path='/contact' element={<Contact />} />
             <Route path='/FAQ' element={<FAQ/>}/>
             <Route path='/JoinUs' element={<JoinUs/>}></Route>
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/history" element={<History />} />
+            
           </Routes>
+          <Footer />
         </div>
       </Router>
     </AuthProvider>
