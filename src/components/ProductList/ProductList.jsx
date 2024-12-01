05aceab14d7dd0b9a611122cd83e62d432031144
juanjose <juanjoperez2121@gmail.com>
2024-12-01 17:33:35 -0500

    Se intenta solucionar problemas del header

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -13,7 +13,6 @@ const ProductList = ({ title, products = [], onError }) => {
       product.id &&
       product.name &&
       product.imageUrls &&
-      product.imageUrls.length > 0 &&
       typeof product.price === 'number' &&
       product.price >= 0
     )
@@ -87,8 +86,8 @@ const ProductList = ({ title, products = [], onError }) => {
       <h2 className="product-list-title">{title}</h2>
       <div className="product-list">
         {uniqueProducts.map((product) => (
-          <Link key={product.id} to={'/product-description/' + product.id}>
-            <div key={product.id} className="product-card">
+          <div key={product.id} className="product-card">
+            <Link key={product.id} to={'/product-description/' + product.id}>
               <img
                 src={product.imageUrls[0] || '/placeholder-image.png'}
                 alt={product.name}
@@ -96,62 +95,60 @@ const ProductList = ({ title, products = [], onError }) => {
               />
 
               <h3 className="product-name">{product.name}</h3>
-              <div className="product-rating-container">
-                <div className="product-rating">
-                  {[...Array(5)].map((_, i) => (
-                    <span
-                      key={i}
-                      className={
-                        i < (product.rating || 0) ? 'star filled' : 'star'
-                      }
-                    >
-                      ★
-                    </span>
-                  ))}
-                </div>
-                <p className="product-rating-value">
-                  {product.rating
-                    ? product.rating.toFixed(2)
-                    : 'Sin calificación'}
-                </p>
-              </div>
-              <div className="product-price-container">
-                <p className="product-price">
-                  COP {formatPrice(product.price)}
-                </p>
-                {product.discount > 0 && (
-                  <p className="product-discount">
-                    ${formatPrice(product.discount)}
-                  </p>
-                )}
-              </div>
-              <div className="product-sold-container">
-                <p className="title-ventas">Ventas</p>
-                <p className="product-sold">{product.totalSales || 0}</p>
-              </div>
-              <div className="product-actions">
-                <button
-                  className="add-to-cart-button cart"
-                  aria-label="Agregar al carrito"
-                  onClick={() => addToCart(product)}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
+            </Link>
+            <div className="product-rating-container">
+              <div className="product-rating">
+                {[...Array(5)].map((_, i) => (
+                  <span
+                    key={i}
+                    className={
+                      i < (product.rating || 0) ? 'star filled' : 'star'
+                    }
                   >
-                    <circle cx="9" cy="21" r="1" />
-                    <circle cx="20" cy="21" r="1" />
-                    <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6" />
-                  </svg>
-                </button>
+                    ★
+                  </span>
+                ))}
               </div>
+              <p className="product-rating-value">
+                {product.rating
+                  ? product.rating.toFixed(2)
+                  : 'Sin calificación'}
+              </p>
             </div>
-          </Link>
+            <div className="product-price-container">
+              <p className="product-price">COP {formatPrice(product.price)}</p>
+              {product.discount > 0 && (
+                <p className="product-discount">
+                  ${formatPrice(product.discount)}
+                </p>
+              )}
+            </div>
+            <div className="product-sold-container">
+              <p className="title-ventas">Ventas</p>
+              <p className="product-sold">{product.totalSales || 0}</p>
+            </div>
+            <div className="product-actions">
+              <button
+                className="add-to-cart-button cart"
+                aria-label="Agregar al carrito"
+                onClick={() => addToCart(product)}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <circle cx="9" cy="21" r="1" />
+                  <circle cx="20" cy="21" r="1" />
+                  <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6" />
+                </svg>
+              </button>
+            </div>
+          </div>
         ))}
       </div>
     </div>
